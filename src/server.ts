3958854b60356b8e37af6b7b30b6a16efe937722
jimhu79 <jimhu79@CORP.AMAZONWORKSPACES.COM>
2020-10-19 16:07:23 -0400

    initial

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -29,14 +29,35 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
+  app.get( "/filteredimage", async ( req, res ) => {
+    let { image_url } = req.query;
+
+    if ( !image_url ) {
+      return res.status(400)
+                .send(`image url is required`);
+    }
+
+    try {
+      let filteredpath = await filterImageFromURL(image_url);
+      return res.status(200).sendFile(filteredpath, () => {
+        deleteLocalFiles([filteredpath])
+      });
+    } catch (error) {
+      console.log("error caught by server");
+      return res.status(400).send('unable to filter content at image_url');
+    }
+
+
+  });
+
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
-  
+
 
   // Start the Server
   app.listen( port, () => {
